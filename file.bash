b98a47eb9f40d14690af8616670b1ca79b68c340
Sajal Nema <78894677+Sajal687@users.noreply.github.com>
2024-04-21 12:32:29 +0530

    Update file.bash

--- file.bash
+++ file.bash
@@ -3,9 +3,7 @@
 set -e
 set -x
 
-cd "$GITHUB_WORKSPACE/JavaScript_Mini_Project"  # Set working directory to the project directory
 mkdir mydir
 find . -name mydir 
 echo "$(pwd)"
 echo 'directory is successfully created'
-
